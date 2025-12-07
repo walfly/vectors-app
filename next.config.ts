b59611e39266ed62ccfa9c8a7081f0aa7b343a6a
Walker Flynn <walkerflynn@Walkers-MacBook-Pro.local>
2025-12-07 10:44:15 -0800

    more modern config

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ["sharp", "onnxruntime-node"],
-  },
-  webpack: (config) => {
-    // Ignore node-specific modules when bundling for the browser
-    // https://webpack.js.org/configuration/resolve/#resolvealias
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      sharp$: false,
-      "onnxruntime-node$": false,
-    };
-    return config;
-  },
+  serverExternalPackages: ['sharp', 'onnxruntime-node'],
 };
 
 export default nextConfig;
