649ee6319f2fe93d1ca0aba9b383ac6760d03508
walker <walker.d.flynn@gmail.com>
2025-12-07 16:07:39 -0800

    configs

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
-  serverExternalPackages: ['sharp', 'onnxruntime-node', '@huggingface/transformers'],
+  /* config options here */
 };
 
 export default nextConfig;
